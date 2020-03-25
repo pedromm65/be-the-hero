653b968790663f3cee36553f98e141470c030dfe
Pedro Lima <pedromm65@hotmail.com.br>
2020-03-25 05:32:46 -0300

    Back-end da aplicacao

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,28 @@
+const connection = require('../database/conection')
+const crypto = require('crypto')
+ 
+
+module.exports = {
+    async index(req, res) {
+        const ongs = await connection('ongs').select('*')
+    
+        return res.json(ongs)
+    },
+
+    async create(req, res) {
+        const { name, email, whatsapp, city, uf } = request.body
+
+        const id = crypto.randomBytes(4).toString('HEX')
+    
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        })
+        
+        return response.json({ id })
+    }
+}
