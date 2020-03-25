672bc830c27fc5d779ecbda30bd44c1f524989b3
Pedro Lima <pedromm65@hotmail.com.br>
2020-03-25 16:45:25 -0300

    Front-end da aplicacao

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -10,7 +10,7 @@ module.exports = {
     },
 
     async create(req, res) {
-        const { name, email, whatsapp, city, uf } = request.body
+        const { name, email, whatsapp, city, uf } = req.body
 
         const id = crypto.randomBytes(4).toString('HEX')
     
@@ -23,6 +23,6 @@ module.exports = {
             uf
         })
         
-        return response.json({ id })
+        return res.json({ id })
     }
 }
